75e4d0b59817d6441a73368e180a675d78398728
Gabriela Damasceno <mgabrieladamasceno@gmail.com>
2021-08-20 22:22:58 -0400

    Limpeza de código

--- PAIC/classe_disco.py
+++ PAIC/classe_disco.py
@@ -5,6 +5,8 @@ import math
 resolution = [1200, 800]
 
 '''Classe Disco'''
+
+
 class Disco:
     def __init__(self, cor, xpos, ypos, xvel, yvel, rad, m, yacc):
         self.cor = cor
@@ -21,36 +23,30 @@ class Disco:
         norma = math.sqrt((self.dx ** 2) + (self.dy ** 2))
         return norma
 
-
     def debug(self, disco):
-        dx = (self.x - disco.x)
-        dy = (self.y - disco.y)
-        distancia = math.sqrt(dx * dx + dy * dy)
-        diametro = (self.radius + disco.radius)
-        energy_self = 0.5* (self.mass) * (self.velo_module())
-        energy_disco = 0.5* (disco.mass) * (disco.velo_module())
+        energy_self = 0.5 * self.mass * self.velo_module()
+        energy_disco = 0.5 * disco.mass * disco.velo_module()
         print(f"Self: Posição X = {self.x} Posição Y = {self.y}")
         print(f"VelX = {self.dx} VelY = {self.dy} raio = {self.radius}")
         print(f"Disco: Posição X = {disco.x} Posição Y = {disco.y}")
         print(f"VelX = {disco.dx} VelY = {disco.dy} raio = {disco.radius}")
         print(f"Energia Self = {energy_self} e Energia_disco = {energy_disco} ")
 
-
     def colision(self, disco):
         dx = (self.x - disco.x)
         dy = (self.y - disco.y)
-        distancia = math.sqrt(dx*dx + dy*dy)
+        distancia = math.sqrt(dx * dx + dy * dy)
         diametro = (self.radius + disco.radius)
         if distancia <= diametro:
-            '''print("****ANTES DA COLISÃO****")
-            self.debug(disco)'''
+            print("****ANTES DA COLISÃO****")
+            self.debug(disco)
             self.dx *= -1
             self.dy *= -1
             disco.dx *= -1
             disco.dy *= -1
-            '''print("\n****DEPOIS DA COLISÃO****")
+            print("\n****DEPOIS DA COLISÃO****")
             self.debug(disco)
-            print("-------------------------------------------")'''
+            print("-------------------------------------------")
 
     def draw(self, screen):
         pygame.draw.circle(screen, self.cor, (int(self.x), int(self.y)), self.radius)
@@ -58,9 +54,9 @@ class Disco:
     def update(self, gameObjects, gravidade):
         self.ay = gravidade
         for disco in gameObjects:
-            self.dy += (1/30)*self.ay
+            self.dy += (1 / 30) * self.ay
             self.x += self.dx
-            self.y += self.dy*(1/30)
+            self.y += self.dy * (1 / 30)
 
             if self.x - self.radius <= 0 or self.x + self.radius >= resolution[0]:
                 self.dx *= -1
@@ -70,12 +66,3 @@ class Disco:
             for disc in gameObjects:
                 if self != disc:
                     self.colision(disc)
-
-
-
-
-
-
-
-
-
--- PAIC/classe_game.py
+++ PAIC/classe_game.py
@@ -7,13 +7,13 @@ from scipy.stats import maxwell
 import matplotlib.pyplot as plt
 import numpy as np
 
-
 '''Quantidade partículas'''
 print("Bem-vindo a simulação!")
 num = int(input("Número de partículas entre 10 e 200: "))
 if 9 < num < 201:
     print("\nPara analisar o gráfico de Histogramas pressione F1 \nPara analisar o gráfico de Dispersão pressione F2")
-    print("\nPara alterar o potencial gravitacional tecle: \n[F4] para dobrar \n[F5] para quadruplicar \n[F6] para dividir por 1/2 \n[F7] para dividir por 1/4")
+    print("\nPara alterar o potencial gravitacional tecle: \n[F4] para dobrar "
+          "\n[F5] para quadruplicar \n[F6] para dividir por 1/2 \n[F7] para dividir por 1/4")
 
     '''Resolução da Tela'''
     resolution = [1200, 800]
@@ -28,9 +28,12 @@ if 9 < num < 201:
     BROWN = [128, 0, 0]
 
     '''Classe Game'''
+
+
     class Game:
 
         gravidade = 1
+
         def __init__(self):
             pygame.init()
 
@@ -38,37 +41,36 @@ if 9 < num < 201:
             self.clock = pygame.time.Clock()
             self.gameObjects = []
 
-        def operacao(self, g, m):
-            gr = g*m
-            #print(gr)
+        @staticmethod
+        def operacao(g, m):
+            gr = g * m
             if 0 < gr < 16:
                 return gr
             else:
                 print("Limite gravitacional alcançado, faça outra operação!")
                 return g
 
-
         def load(self):
             matriz = []
-            overlapping = False
             i = 0
             protecao = 0
             while len(matriz) < num:
-                rad = 5
+                rad = 8
                 mass = 1
-                disco = Disco(choice([BLACK, GREEN, RED, BROWN]), randrange(rad, (resolution[0] - rad)), randrange(rad, (resolution[1] - rad)), 0.1, 0.1, rad, mass, self.gravidade)
+                disco = Disco(choice([BLACK, GREEN, RED, BROWN]), randrange(rad, (resolution[0] - rad)),
+                              randrange(rad, (resolution[1] - rad)), 0.1, 0.1, rad, mass, self.gravidade)
 
                 overlapping = False
                 for j in range(len(matriz)):
                     outro = matriz[j]
                     dx = (disco.x - outro.x)
                     dy = (disco.y - outro.y)
-                    distancia = math.sqrt(dx*dx + dy*dy)
+                    distancia = math.sqrt(dx * dx + dy * dy)
                     if distancia < (disco.radius + outro.radius):
                         overlapping = True
                         break
 
-                if overlapping == False:
+                if not overlapping:
                     matriz.append(disco)
                     self.gameObjects.append(disco)
 
@@ -111,7 +113,6 @@ if 9 < num < 201:
                     self.gravidade = self.operacao(self.gravidade, 0.25)
                     print(f"A gravidade agora é {self.gravidade} vezes a inicial e um quarto da anterior!")
 
-
         def run(self):
             while True:
                 self.handleEvents()
@@ -123,26 +124,21 @@ if 9 < num < 201:
                 self.clock.tick(30)
                 pygame.display.flip()
 
-
-        def histograma(self):
+        @staticmethod
+        def histograma():
             fig, ax = plt.subplots(1, 1)
 
             # Parâmetros
             x = np.linspace(maxwell.ppf(0.01), maxwell.ppf(0.99), num)
             ax.plot(x, maxwell.pdf(x), 'g-', lw=5, alpha=0.6, label='Curva de Maxwell-Boltzman')
-            y = maxwell.pdf(x)
 
             # Parametros Histograma
             r = maxwell.rvs(size=1000)
-            ax.hist(r, density=True, histtype='stepfilled', alpha=0.2, color=None, label= f"Histograma com {num} partículas")
+            ax.hist(r, density=True, histtype='stepfilled', alpha=0.2, color=None,
+                    label=f"Histograma com {num} partículas")
 
             # Plotar Gráfico
-            ax.legend(loc='upper right', frameon= True)
-            '''print("Percentual de pontos de função: ")
-            print(x)
-            print("--------------------------------------------------------------------")
-            print("Probabilidade de densidade de função: ")
-            print(y)'''
+            ax.legend(loc='upper right', frameon=True)
             plt.title("Curva de Maxwell-Boltzman", size=11)
             plt.xlabel("Velocidade (km/s)", size=10)
             plt.ylabel("Densidade de Probabilidade (s/km)", size=10)
@@ -156,11 +152,11 @@ if 9 < num < 201:
                 posY.append(disco.y)
                 X.append(disco.x)
 
-            #Inversão do eixo Y
-            lista = [resolution[1]]*num
+            # Inversão do eixo Y
+            lista = [resolution[1]] * num
             a = np.array(posY)
             b = np.array(lista)
-            Y = -1*(a - b)
+            Y = -1 * (a - b)
 
             '''print("---------------------------------------------------------")
             print(f"Posição Y {Y}")
@@ -170,7 +166,7 @@ if 9 < num < 201:
             colors = np.random.rand(num)
             plt.scatter(X, Y, c=colors, label=f"{num} partículas e {self.gravidade} vezes o potencial inicial")
             plt.title("Gráfico de Dispersão de partículas", size=11)
-            ax.legend(loc = 'best', borderpad=1)
+            ax.legend(loc='best', borderpad=1)
             plt.xlabel("Posição X", size=9)
             plt.ylabel("\nPosição Y", size=9)
             plt.show()
@@ -191,7 +187,7 @@ if 9 < num < 201:
             tamanho = len(Y)
 
             # quantidade de Classes (bins)
-            cl = int(round(tamanho ** (1/2), 0))
+            cl = int(round(tamanho ** (1 / 2), 0))
 
             plt.title("Histograma de Partículas x Altura", size=11)
             plt.xlabel("Alturas")
